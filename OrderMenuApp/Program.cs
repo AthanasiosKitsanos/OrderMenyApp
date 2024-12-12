f5b06f1046c2a6ed89ccff9bf37d784bc115939e
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-12 20:22:42 +0200

    Written code for tests.

--- OrderMenuApp/Program.cs
+++ OrderMenuApp/Program.cs
@@ -3,14 +3,21 @@ using OrderMenuApp.Models;
 
 namespace OrderMenuApp;
 
-class MainProgram
+
+partial class MainProgram
 {
     static void Main(string[] args)
     {
-            
+        ConfigureConsole();
 
-            
+        Pizza pizza = new();
+        Burger burger = new();
+        Pasta pasta = new();
+        Salad salad = new();
+        Sushi sushi = new();
 
+        pizza.TypeOfProduct.Add("Margarita", 10.5);
+        pizza.ShowListOfSelection();
+        
     }
 }
-
