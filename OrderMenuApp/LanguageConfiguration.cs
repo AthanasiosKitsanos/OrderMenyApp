05e1505da94af362f6a42db7cbb6a7dc855baa30
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-12 20:21:15 +0200

    language Configuration for US output on currency.

---
+++ OrderMenuApp/LanguageConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace OrderMenuApp;
+
+partial class MainProgram
+{
+    private static void ConfigureConsole(string culture = "en-US")
+    {
+        CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+    }
+}
+
