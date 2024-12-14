505b980985fb01e51d0778758b5173deccbe414c
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-14 13:46:22 +0200

    Added a method to create a table border

--- OrderMenuApp/Program.Helpers.cs
+++ OrderMenuApp/Program.Helpers.cs
@@ -1,10 +1,19 @@
 ﻿using OrderMenuApp.Models;
+using Spectre.Console;
 using System;
 
 namespace OrderMenuApp;
 
 partial class MainProgram
 {
-  
+    private static void Createtable(Table table)
+    {
+        table.AddColumn(new TableColumn("[Yellow]List Menu[/]"));
+        table.AddRow($"1. {nameof(Pizza)}");
+        table.AddRow($"2. {nameof(Burger)}");
+        table.AddRow($"3. {nameof(Pasta)}");
+        table.AddRow($"4. {nameof(Salad)}");
+        table.AddRow($"5. {nameof(Sushi)}");
+    }
 }
 
