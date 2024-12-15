2ac2264d86f72f26070026e44947cfc724d5c07d
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-15 13:26:26 +0200

    Added a method to create a "Add the items and the prices in a Receipt"

--- OrderMenuApp/Models/OrderDish.cs
+++ OrderMenuApp/Models/OrderDish.cs
@@ -4,12 +4,43 @@ namespace OrderMenuApp.Models;
 
 public class OrderDish
 {
-    public static List<double> AmountList = new();
+    public static Dictionary<string, double> DetailedDictionary = new();
 
-    // Adds the Dictionary Values into a List and then adds each value of the List
+    public static List<double> PriceList = new(); 
+
+    //Adds the Dictionary Values into a List and then adds each value of the List
     public static List<double> AddPriceToList(int userChoice, Dictionary<string, double> aDict)
     {
-        AmountList.Add(aDict.Values.ElementAt(userChoice - 1));
-        return AmountList;
+        PriceList.Add(aDict.Values.ElementAt(userChoice - 1));
+        return PriceList;
+    }
+
+    // This method takes the user's choice and adds it as a key in the dictionary, but if the key exists, it replaces it and updates the value.
+    public static Dictionary<string, double> BuildReceipt(int userChoice, Dictionary<string, double> aDict)
+    {
+        string dishName = aDict.Keys.ElementAt(userChoice - 1);
+        double dishPrice = aDict.Values.ElementAt(userChoice - 1);
+
+        string? existingKey = DetailedDictionary.Keys.FirstOrDefault(key => key == dishName || key.EndsWith($"x {dishName}"));
+
+        if (existingKey is not null)
+        {
+            int count = 1;
+            if (existingKey.Contains("x"))
+            {
+                count = int.Parse(existingKey.Split('x')[0]);
+            }
+
+            count++;
+            string newKey = $"{count}x {dishName}";
+
+            DetailedDictionary.Remove(existingKey);
+            DetailedDictionary[newKey] = count * dishPrice;
+        }
+        else
+        {
+            DetailedDictionary[dishName] = dishPrice;
+        }
+        return DetailedDictionary;
     }
 }
