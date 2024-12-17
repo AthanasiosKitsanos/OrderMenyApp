b467bd560154a22c6fee926f30c6a145e02ae99e
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-17 21:36:36 +0200

    Added a file with an EventHandler.

---
+++ OrderMenuApp/Events/EventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OrderMenuApp.Events;
+
+public static class HandlerOfEvents
+{
+    public static event EventHandler<ConsoleKeyInfo> KeyPress;
+
+    public static void OnKeyPressedOnMenu(ConsoleKeyInfo keyinfo)
+    {
+        KeyPress?.Invoke(null, keyinfo);
+    }
+
+    public static void OnKeyPressedInTypes(ConsoleKeyInfo keyinfo)
+    {
+        KeyPress?.Invoke(null, keyinfo);
+    }
+}
+
