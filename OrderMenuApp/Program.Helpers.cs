7f92debc1fe949cbf07744f66134460794dd7d95
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-17 21:47:24 +0200

    Added methods for navigation and a second subscriber to the EventHandler<ConsoleKeyInfo>

--- OrderMenuApp/Program.Helpers.cs
+++ OrderMenuApp/Program.Helpers.cs
@@ -1,33 +1,90 @@
-﻿using OrderMenuApp.Models;
+﻿using OrderMenuApp.Events;
+using OrderMenuApp.Models;
 using Spectre.Console;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace OrderMenuApp;
 
 partial class MainProgram
 {
-    private static void CreateTable(Table table)
+    private static int currentItemIndex = 1;
+    private static int currentTableIndex = 1;
+    private static bool lastExcecution = false;
+
+    private static Table table = new();
+    private static Pizza pizza = new();
+    private static Burger burger = new();
+    private static Pasta pasta = new();
+    private static Salad salad = new();
+    private static Sushi sushi = new();
+    private static Dictionary<string, double> orderTypeDictionary = new();
+    private static Dictionary<string, double> ListOfOrderedDishes = new();
+    private static void CreatePizzaTable(Table table)
     {
+        table = new();
+        Console.Clear();
+        table.AddColumn(new TableColumn("[Yellow]List Menu[/]"));
+        table.AddRow($"[underline]1. {nameof(Pizza)}[/]");
+        table.AddRow($"2. {nameof(Burger)}");
+        table.AddRow($"3. {nameof(Pasta)}");
+        table.AddRow($"4. {nameof(Salad)}");
+        table.AddRow($"5. {nameof(Sushi)}");
+        AnsiConsole.Write(table);
+        Console.WriteLine("Navigate up and down with arrows. Press ENTER to select or Esc to go back or Q to stop ordering.");
+    }
+    private static void CreateBurgerTable(Table table)
+    {
+        table = new();
+        Console.Clear();
+        table.AddColumn(new TableColumn("[Yellow]List Menu[/]"));
+        table.AddRow($"1. {nameof(Pizza)}");
+        table.AddRow($"[underline]2. {nameof(Burger)}[/]");
+        table.AddRow($"3. {nameof(Pasta)}");
+        table.AddRow($"4. {nameof(Salad)}");
+        table.AddRow($"5. {nameof(Sushi)}");
+        AnsiConsole.Write(table);
+        Console.WriteLine("Navigate up and down with arrows. Press ENTER to select or Esc to go back or Q to stop ordering.");
+    }
+    private static void CreatePastaTable(Table table)
+    {
+        table = new();
+        Console.Clear();
+        table.AddColumn(new TableColumn("[Yellow]List Menu[/]"));
+        table.AddRow($"1. {nameof(Pizza)}");
+        table.AddRow($"2. {nameof(Burger)}");
+        table.AddRow($"[underline]3. {nameof(Pasta)}[/]");
+        table.AddRow($"4. {nameof(Salad)}");
+        table.AddRow($"5. {nameof(Sushi)}");
+        AnsiConsole.Write(table);
+        Console.WriteLine("Navigate up and down with arrows. Press ENTER to select or Esc to go back or Q to stop ordering.");
+    }
+    private static void CreateSaladTable(Table table)
+    {
+        table = new();
+        Console.Clear();
+        table.AddColumn(new TableColumn("[Yellow]List Menu[/]"));
+        table.AddRow($"1. {nameof(Pizza)}");
+        table.AddRow($"2. {nameof(Burger)}");
+        table.AddRow($"3. {nameof(Pasta)}");
+        table.AddRow($"[underline]4. {nameof(Salad)}[/]");
+        table.AddRow($"5. {nameof(Sushi)}");
+        AnsiConsole.Write(table);
+        Console.WriteLine("Navigate up and down with arrows. Press ENTER to select or Esc to go back or Q to stop ordering.");
+    }
+    private static void CreateSushiTable(Table table)
+    {
+        table = new();
+        Console.Clear();
         table.AddColumn(new TableColumn("[Yellow]List Menu[/]"));
         table.AddRow($"1. {nameof(Pizza)}");
         table.AddRow($"2. {nameof(Burger)}");
         table.AddRow($"3. {nameof(Pasta)}");
         table.AddRow($"4. {nameof(Salad)}");
-        table.AddRow($"5. {nameof(Sushi)}");
-    }
-
-    private static bool CheckUserInput(ConsoleKey number)
-    {
-        if (number != ConsoleKey.D1 && number != ConsoleKey.NumPad1 &&
-                   number != ConsoleKey.D2 && number != ConsoleKey.NumPad2 &&
-                   number != ConsoleKey.D3 && number != ConsoleKey.NumPad3 &&
-                   number != ConsoleKey.D4 && number != ConsoleKey.NumPad4 &&
-                   number != ConsoleKey.D5 && number != ConsoleKey.NumPad5)
-        {
-            return false;
-        }
-        return true;
+        table.AddRow($"[underline]5. {nameof(Sushi)}[/]");
+        AnsiConsole.Write(table);
+        Console.WriteLine("Navigate up and down with arrows. Press ENTER to select or Esc to go back or Q to stop ordering.");
     }
 
     private static void DictionaryInfo(Dictionary<string, double> dictionary)
@@ -47,30 +104,149 @@ partial class MainProgram
         Console.WriteLine($"{dateTime:HH:mm:ss dd/MM/yyyy}");
     }
 
-    private static Dictionary<string, double> ReturnDictionary(ConsoleKey num, Dictionary<string, double> dictionary)
+    public static void CreateTableByIndex(int tableindex)
     {
-        dictionary = num switch
+        switch (tableindex)
         {
-            ConsoleKey.D1 or ConsoleKey.NumPad1 => pizza.ShowListOfSelection(),
-            ConsoleKey.D2 or ConsoleKey.NumPad2 => burger.ShowListOfSelection(),
-            ConsoleKey.D3 or ConsoleKey.NumPad3 => pasta.ShowListOfSelection(),
-            ConsoleKey.D4 or ConsoleKey.NumPad4 => salad.ShowListOfSelection(),
-            ConsoleKey.D5 or ConsoleKey.NumPad5 => sushi.ShowListOfSelection()
-        };
-        return dictionary;
+            case 1:
+                CreatePizzaTable(table);
+                break;
+            case 2:
+                CreateBurgerTable(table);
+                break;
+            case 3:
+                CreatePastaTable(table);
+                break;
+            case 4:
+                CreateSaladTable(table);
+                break;
+            case 5:
+                CreateSushiTable(table);
+                break;
+        }
     }
 
-    private static int ReturnChoiceToInt(ConsoleKey num, int userChoice)
+    private static void NavigateToNextTable()
     {
-        userChoice = num switch
+        if(currentTableIndex < 5)
         {
-            ConsoleKey.D1 or ConsoleKey.NumPad1 => 1,
-            ConsoleKey.D2 or ConsoleKey.NumPad2 => 2,
-            ConsoleKey.D3 or ConsoleKey.NumPad3 => 3,
-            ConsoleKey.D4 or ConsoleKey.NumPad4 => 4,
-            ConsoleKey.D5 or ConsoleKey.NumPad5 => 5,
+            currentTableIndex++;
+            CreateTableByIndex(currentTableIndex);
+        }
+        else if(currentTableIndex == 5)
+        {
+            currentTableIndex = 1;
+            CreateTableByIndex(currentTableIndex);
+        }
+    }
+
+    private static void NavigateToPreviousTable()
+    {
+        if(currentTableIndex > 1)
+        {
+            currentTableIndex--;
+            CreateTableByIndex(currentTableIndex);
+        }
+        else if(currentTableIndex == 1)
+        {
+            currentTableIndex = 5;
+            CreateTableByIndex(currentTableIndex);
+        }
+    }
+    private static void DisplayTypeOfDishTable()
+    {
+        Console.Clear();
+        switch(currentTableIndex)
+        {
+            case 1:
+                pizza.ShowListOfSelection(currentItemIndex);
+                break;
+            case 2:
+                burger.ShowListOfSelection(currentItemIndex);
+                break;
+            case 3:
+                pasta.ShowListOfSelection(currentItemIndex);
+                break;
+            case 4:
+                salad.ShowListOfSelection(currentItemIndex);
+                break;
+            case 5:
+                sushi.ShowListOfSelection(currentItemIndex);
+                break;
         };
-        return userChoice;
+    }
+
+    private static void NavTypeOfDishNext()
+    {
+        if (currentItemIndex < 5)
+        {
+            currentItemIndex++;
+            DisplayTypeOfDishTable();
+        }
+        else if (currentItemIndex == 5)
+        {
+            currentItemIndex = 1;
+            DisplayTypeOfDishTable();
+        }
+    }
+
+    private static void NavTypeOfDishPrevious()
+    {
+        if (currentItemIndex > 1)
+        {
+            currentItemIndex--;
+            DisplayTypeOfDishTable();
+        }
+        else if (currentItemIndex == 1)
+        {
+            currentItemIndex = 5;
+            DisplayTypeOfDishTable();
+        }
+    }
+
+    private static void HandleKeyPress(ConsoleKeyInfo keyInfo, Dictionary<string, double> currentMenu)
+    {
+        if(keyInfo.Key == ConsoleKey.DownArrow)
+        {
+            NavTypeOfDishNext();
+        }
+        else if(keyInfo.Key == ConsoleKey.UpArrow)
+        {
+            NavTypeOfDishPrevious();
+        }
+        else if(keyInfo.Key == ConsoleKey.Enter)
+        {
+            
+        }
+        else if(keyInfo.Key == ConsoleKey.Escape)
+        {
+            currentItemIndex = 1;
+            Console.Clear();
+            CreateTableByIndex(currentTableIndex);
+            lastExcecution = false;
+        }
+        else if(keyInfo.Key == ConsoleKey.Q)
+        {
+            Console.Clear();
+            FinalReceipt(ListOfOrderedDishes);
+            Environment.Exit(0);
+        }
+
+    }
+    public static async Task ListeningForKey()
+    {
+        while (true)
+        {
+            var key = await Task.Run(() => Console.ReadKey(true));
+            if (!lastExcecution)
+            {
+                HandlerOfEvents.OnKeyPressedOnMenu(key);
+            }
+            else
+            {
+                HandleKeyPress(key, orderTypeDictionary);
+            }
+        };
     }
 }
 
