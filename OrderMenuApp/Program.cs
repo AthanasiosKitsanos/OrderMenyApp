fe9765044d84e113239737fc6fe620aa34619613
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-17 21:46:16 +0200

    Chenged the code for use of anonymous labda expresion of event handler and event subscriber.

--- OrderMenuApp/Program.cs
+++ OrderMenuApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 using OrderMenuApp.Models;
+using OrderMenuApp.Events;
 using Spectre.Console;
 using Spectre.Console.Rendering;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -9,67 +10,41 @@ namespace OrderMenuApp;
 
 
 partial class MainProgram
-{
-    private static Pizza pizza = new();
-    private static Burger burger = new();
-    private static Pasta pasta = new();
-    private static Salad salad = new();
-    private static Sushi sushi = new();
-    static void Main(string[] args)
+{ 
+    static async Task Main(string[] args)
     {
         ConfigureConsole();
+        CreatePizzaTable(table);
 
-
-        Table table = new();
-        CreateTable(table);
-
-        while (true)
+        HandlerOfEvents.KeyPress += (sender, keyInfo) =>
         {
-            AnsiConsole.Write(table);
-
-            Console.WriteLine("\nSelect an item by pressing 1 to 5.");
-            ConsoleKey number;
-            do
+            if (keyInfo.Key == ConsoleKey.DownArrow)
             {
-                number = Console.ReadKey(intercept: true).Key;
-
-            }while(!CheckUserInput(number));
-
-            AnsiConsole.Clear();
-            Dictionary<string, double> orderTypeDictionary = new();
-            orderTypeDictionary = ReturnDictionary(number, orderTypeDictionary);
-            
-            do
+                NavigateToNextTable();
+            }
+            else if (keyInfo.Key == ConsoleKey.UpArrow)
             {
-                number = Console.ReadKey(intercept: true).Key;
-
-            } while (!CheckUserInput(number));
-
-            int userChoice = 0; 
-            userChoice = ReturnChoiceToInt(number, userChoice);
-            
-
-            // This method takes the user's choice and adds it as a key in the dictionary, but if the key exists, it replaces it and updates the value.
-            Dictionary<string, double> ListOfOrderedDishes = OrderDish.BuildReceipt(userChoice, orderTypeDictionary);
-
-            Console.WriteLine("Running Order:");
-            DictionaryInfo(ListOfOrderedDishes);
-
-
-            // This method takes the user's choice, uses it as a value for the Dictionary and then add that value in a double type List
-            OrderDish.AddPriceToList(userChoice, orderTypeDictionary);
-            Console.WriteLine($"Total Amount: {OrderDish.PriceList.Sum():C}");
-
-            Console.WriteLine("Press Any Key to continue ordering or ESC to stop ordering.");
-            number = Console.ReadKey(intercept: true).Key;
-            if (number == ConsoleKey.Escape)
+                NavigateToPreviousTable();   
+            }
+            else if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                DisplayTypeOfDishTable();
+                lastExcecution = true;
+            }
+            else if(keyInfo.Key == ConsoleKey.Escape)
+            {
+                CreateTableByIndex(currentTableIndex);
+                lastExcecution = false;
+            }
+            else if(keyInfo.Key == ConsoleKey.Q)
             {
                 Console.Clear();
                 FinalReceipt(ListOfOrderedDishes);
-                break;
+                Environment.Exit(0);
             }
-            Console.Clear();
-        }
+        };
 
+        await Task.Run(() => ListeningForKey());
     }
 }
+
