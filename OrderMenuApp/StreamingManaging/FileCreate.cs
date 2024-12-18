6c48b3922ad741cc64d3081f11e8c46346ce9d46
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2024-12-18 18:44:52 +0200

    Methods to create a folder and a .txt file that saves the results.

---
+++ OrderMenuApp/StreamingManaging/FileCreate.cs
@@ -0,0 +1,55 @@
+﻿using OrderMenuApp.Models;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderMenuApp.StreamingManaging;
+
+public class FileCreate
+{
+    public static string CreateNewFolder()
+    {
+        string folderPath = @"C:\Users\PC\source\repos\Portofolio\MenuApplication\OrderMenuApp\Receipt Folder";
+        if(!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+        return folderPath;
+    }
+    public static void CreateNewFile()
+    {
+        Table table = new Table();
+        string fileName = Combine(CreateNewFolder(), "Receipts.txt");
+        if(!File.Exists(fileName))
+        {
+            using(StreamWriter fileWriter = File.CreateText(fileName))
+            {
+
+                Console.WriteLine("Receipt:");
+                foreach (KeyValuePair<string, double> item in MainProgram.ListOfOrderedDishes)
+                {
+                    fileWriter.WriteLine($"\n{item.Key}: ${item.Value:F2}");
+                }
+                fileWriter.WriteLine($"\nTotal Amount: {OrderDish.PriceList.Sum():C}");
+                fileWriter.WriteLine($"{MainProgram.dateTime:HH:mm:ss dd/MM/yyyy}");
+            }
+        }
+        else
+        {
+            using (StreamWriter fileWriter = new StreamWriter(fileName, true))
+            {
+                fileWriter.WriteLine("\n\nReceipt:");
+                foreach (KeyValuePair<string, double> item in MainProgram.ListOfOrderedDishes)
+                {
+                    fileWriter.WriteLine($"\n{item.Key}: ${item.Value:F2}");
+                }
+                fileWriter.WriteLine($"\nTotal Amount: {OrderDish.PriceList.Sum():C}");
+                fileWriter.WriteLine($"{MainProgram.dateTime:HH:mm:ss dd/MM/yyyy}");
+            }
+        }
+    }
+}
+
